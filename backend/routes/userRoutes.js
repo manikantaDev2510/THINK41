ab63ec0de1d1749e96ea135e06fbd3abb1eb16de
manikantaDev2510 <balu25102002@gmail.com>
2025-07-26 12:42:23 +0530

    Complete Milestone 4 - Core Chat API

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,7 +8,6 @@ router.post('/', async (req, res) => {
         const { userId, name, email } = req.body;
         const user = await User.create({ userId, name, email });
         res.status(201).json(user);
-
     } catch (error) {
         console.error("Error creating user:", error.message);
         res.status(500).json({ error: "Failed to create user" });
